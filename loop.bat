02537041472e29532334333a91b1e6582cf98d4f
HighM <68829949+HighM@users.noreply.github.com>
2021-08-23 01:53:26 +0200

    OK.

--- loop.bat
+++ loop.bat
@@ -4,5 +4,7 @@ tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Make sure your ngrok 
 :check
 ping 127.0.0.1 > null
 cls
-echo This just keeps me running.
+echo Username: TerryDavis
+echo Password: DotCentral@V1
+echo Login to your RDP!
 goto check
